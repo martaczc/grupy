73d1322262d05966faf410bf8e83f3830efbaa8d
Marta Czarnocka-Cieciura <martaczc@interia.pl>
2013-01-07 11:40:01 +0100

    implementation: GroupState, GroupAgentState

--- grupy.py
+++ grupy.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import sys
-sys.path.append('/home/konrad/ICM/cog-abm-tutorial/COG-ABM/src')
+sys.path.append('/home/marta/COG-ABM/src')
 
 import random
 
 from cog_abm.core import Simulation, Agent
 from cog_abm.core.interaction import Interaction #czy tego potrzebuje? jesli tak - do rozrodu.
-from cog_abm.extras.additional_tools import generate_simple_network #czy tego otrzebuje? jesli tak, do wyboru ojca potomstwa.
+from cog_abm.extras.additional_tools import generate_simple_network #czy tego potrzebuje? jesli tak, do wyboru ojca potomstwa.
 from cog_abm.extras.tools import avg
 
 init_num_agents = 100
@@ -18,8 +18,38 @@ b_breed =
 iters = 500000
 dump_freq = iters / 100
 
+class GroupState(object):
+    def __init__(self,n=0,a=[]):
+        self.number=n
+        self.agents=a
+        self.cooperation=sum([agent.state.get_genValue() for agent in self.agents])
+        self.N=self.agents.length
+    def get_agent(self,i)
+        return self.agents[i]
+    def get_number(self)
+        return self.number
+    def get_agentNumber(self)
+        return self.N
+    def get_cooperation(self)
+        return self.cooperation
+    def update(self,a) 
+        self.agents=a
+        self.cooperation=sum([agent.state.get_genValue() for agent in self.agents])
+        self.N=self.agents.length        
+        
+
 class GroupAgentState(object):
-    #TODO
+    def __init__(self,s='m',g=0)
+        self.sex=s
+        self.gen=g
+        #mutations?
+    def get_genValue(self)
+        return self.gen
+    def get_sex(self)
+        return self.sex
+    def get_deathProb(self,cooperation,N)
+        return deathProb(a=a_death,b=b_death,cost=self.gen,cooperation,N)
+        #define deathProb, breedProb (outside)
 
 class GroupInteraction(Interaction): #???
     #TODO
